0739398a465a36691d7f27dcb5c9319438a818cf
Yongbin <rhdydqls1024@naver.com>
2022-07-11 09:47:32 +0900

    ADD : Setting File

---
+++ src/test/java/com/project/mirinae/MirinaeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.mirinae;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MirinaeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
